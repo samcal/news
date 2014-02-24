3fa12cbbfdfa6d478682fad581bbc7eb58610811
Sam Calvert <sam.clvrt@gmail.com>
2014-02-23 20:12:26 -0800

    creating article owner

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,6 +49,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
+		@article.user = current_user
 		@article.save
 		redirect_to @article
 	end
