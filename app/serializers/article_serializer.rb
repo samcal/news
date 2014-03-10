72e9728e3d903da9ffd18d963fa96ba74163beac
Nick Landolfi <nclandolfi@gmail.com>
2014-03-10 11:17:46 -0700

    Add article serializer

---
+++ app/serializers/article_serializer.rb
@@ -0,0 +1,15 @@
+class ArticleSerializer < ActiveModel::Serializer
+  
+  attributes :id, :is_published, :is_draft, :short_title
+
+  has_one :user, embed: :id
+
+  has_one :user, embed: :id
+
+  has_many :comments, embed: :id, include: true
+
+  has_many :similar_articles, embed: :ids
+
+
+
+end
