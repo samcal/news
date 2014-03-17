c65394c2706d8e679934455d26f532ff253fb38c
Nick Landolfi <nclandolfi@gmail.com>
2014-03-17 11:44:17 -0700

    Make tabs spaces in articles_controller.rb:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,71 +1,71 @@
 class ArticlesController < ApplicationController
-	before_action :require_login, except: [:index, :show]
+  before_action :require_login, except: [:index, :show]
 
-	def index
+  def index
     @articles = Article.published.order('created_at DESC').paginate(:page => params[:page])
-	end
+  end
 
-	def show
-		@article = Article.friendly.find(params[:id])
+  def show
+    @article = Article.friendly.find(params[:id])
 
-		if @article.is_draft and @article.user != current_user
-			redirect_to root_path
-		end
+    if @article.is_draft and @article.user != current_user
+      redirect_to root_path
+    end
 
-		if not (@article.is_published or user_signed_in?)
-			redirect_to root_path
-		end
+    if not (@article.is_published or user_signed_in?)
+      redirect_to root_path
+    end
 
-		@current_category = @article.category
-		@similar_articles = @article.similar_articles
-		unless request.path == article_path(@article)
-			redirect_to @article, status: :moved_permanently
-		end
-	end
+    @current_category = @article.category
+    @similar_articles = @article.similar_articles
+    unless request.path == article_path(@article)
+      redirect_to @article, status: :moved_permanently
+    end
+  end
 
-	def edit
-		@article = Article.friendly.find(params[:id])
-		if not (@article.user == current_user or current_user.editor?)
-			redirect_to root_path
-		else
-			render layout: 'dashboard'
-		end
-	end
+  def edit
+    @article = Article.friendly.find(params[:id])
+    if not (@article.user == current_user or current_user.editor?)
+      redirect_to root_path
+    else
+      render layout: 'dashboard'
+    end
+  end
 
-	def update
-		@article = Article.friendly.find(params[:id])
+  def update
+    @article = Article.friendly.find(params[:id])
 
-		if @article.update(article_params)
-			if params[:commit] == 'Save Draft'
-				redirect_to contributor_drafts_path
-			elsif params[:commit] == 'Preview'
-				render 'preview', layout: 'dashboard'
-			elsif params[:commit] == 'Submit'
-				@article.is_draft = false
-				@article.save!
-				redirect_to contributor_drafts_path
-			else
-				redirect_to @article
-			end
-		else
-			render 'edit', layout: 'dashboard'
-		end
-	end
+    if @article.update(article_params)
+      if params[:commit] == 'Save Draft'
+        redirect_to contributor_drafts_path
+      elsif params[:commit] == 'Preview'
+        render 'preview', layout: 'dashboard'
+      elsif params[:commit] == 'Submit'
+        @article.is_draft = false
+        @article.save!
+        redirect_to contributor_drafts_path
+      else
+        redirect_to @article
+      end
+    else
+      render 'edit', layout: 'dashboard'
+    end
+  end
 
-	def new
-		@article = Article.new
-		render layout: 'dashboard'
-	end
+  def new
+    @article = Article.new
+    render layout: 'dashboard'
+  end
 
-	def create
-		@article = Article.new(article_params)
-		@article.user = current_user
-		@article.save
-		redirect_to @article
-	end
-	private
-		def article_params
-			params.require(:article).permit(:title, :author_name, :category_id, :image, :caption, :text)
-		end
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    @article.save
+    redirect_to @article
+  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :author_name, :category_id, :image, :caption, :text)
+    end
 
 end
