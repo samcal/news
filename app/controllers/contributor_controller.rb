86ec313ad47ee29ee581b420e141d20fbd57ea4d
Sam Calvert <sam.clvrt@gmail.com>
2014-03-19 22:09:51 -0700

    extract editor requirement

--- app/controllers/contributor_controller.rb
+++ app/controllers/contributor_controller.rb
@@ -1,6 +1,7 @@
 class ContributorController < ApplicationController
   layout 'dashboard'
   before_action :require_login
+  before_action :require_editor, :only => [:for_review, :staff]
 
   def drafts
     @drafts = Article.where(:user => current_user, :is_published => false)
@@ -11,7 +12,16 @@ class ContributorController < ApplicationController
   end
 
   def for_review
-    redirect_to root_path unless current_user.editor?
     @for_review = Article.where(:is_draft => false, :is_published => false)
+  end
+
+  def staff
+    @staff = User.all
+  end
+
+  private
+
+  def require_editor
+    redirect_to root_path unless current_user.editor?
   end
 end
