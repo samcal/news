8a4e1ad625d39ef1619291c1c722666867871f5f
Sam Calvert <sam.clvrt@gmail.com>
2014-03-19 22:11:03 -0700

    get api spec passing and covering

--- spec/api/v1/articles_controller_spec.rb
+++ spec/api/v1/articles_controller_spec.rb
@@ -1,21 +1,36 @@
 require 'spec_helper'
 
-describe API::V1::ArticlesController do
-  describe "GET index" do
+describe API::V1::ArticlesController, :type => :controller do
+  describe "GET #index" do
+    before do
+      15.times do
+        create(:article)
+      end
+    end
+
     it "responds successfully with an HTTP 200 status code" do
-      get api_v1_articles_path
+      get :index
       expect(response).to be_success
       expect(response.status).to eq(200)
+    end
+
+    it 'paginates when asked to' do
+      get :index, :page => 1
+      expect(assigns(:articles).length).to eq(12)
+    end
+
+    it 'returns all articles by default' do
+      get :index
+      expect(assigns(:articles).length).to eq(15)
     end
   end
 
   describe "GET #show" do
     it "responds successfully with an HTTP 200 status code" do
       article = create(:article)
-      get api_v1_articles_path, id: article.id
+      get :show, id: article.id
       expect(response).to be_success
       expect(response.status).to eq(200)
-      # expect(response.body).to eq(render json: article)
     end
   end
 end
