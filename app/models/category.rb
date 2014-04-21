3c90d22a1cccf7b09689e5317c1c8844c5ab28da
Sam Calvert <sam.clvrt@gmail.com>
2014-04-20 17:05:16 -0700

    retab

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ActiveRecord::Base
-	extend FriendlyId
-	friendly_id :name, use: :slugged
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 
-	has_many :articles
+  has_many :articles
 
   validates_presence_of :name
 end
